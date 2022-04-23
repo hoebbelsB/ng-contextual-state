4b4ab636cdbe19d0699cd69ec3e19270b822e5b1
Julian Jandl <julian@jandl.wien>
2022-04-23 02:45:14 +0200

    initial commit, stage 2 finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,99 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Subject } from 'rxjs';
+import { MovieService } from './movie.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'ng-contextual-state';
+  private searchTerm$ = new Subject<string>();
+  private stop$ = new Subject<void>();
+
+  /** state creation functions **/
+
+  createState = () => this.movieService.getMovies();
+  /* .pipe(
+      map(movies => ({
+       movies: [...movies],
+       })),
+       catchError(() => of({ error: true }))
+     );*/
+
+  createStreamedState = () => this.movieService.streamMovies();
+  /*.pipe(
+      /!*map(movies => ({
+        movies: [...movies],
+      })),
+      catchError(() => of({ error: true })),*!/
+      /!*startWith(undefined),
+      materialize(),
+      map(({ kind, value }) => {
+        if (kind === 'C') {
+          return { complete: true };
+        }
+        if (kind === 'E') {
+          return { error: true };
+        }
+        return value;
+      }),
+      pairwise(),
+      map(([prev, cur]) => {
+        if ((cur as any)?.complete && !!(prev as any).movies) {
+          return { ...cur, ...prev };
+        }
+        return cur;
+      }),*!/
+      // takeUntil(this.stop$),
+      );*/
+
+  createSearchState = () => this.movieService.searchMovies(this.searchTerm$);
+
+  /** template state **/
+  state$ = this.createState();
+  streamedState$ = this.createStreamedState();
+  searchState$ = this.createSearchState();
+
+  /** template triggers **/
+
+  suspenseTrigger$ = new Subject<void>();
+  completeTrigger$ = new Subject<void>();
+  errorTrigger$ = new Subject<void>();
+
+  constructor(private readonly movieService: MovieService) {}
+
+  /** methods called from ui **/
+
+  stop() {
+    this.stop$.next();
+  }
+
+  showError() {
+    this.movieService.withError = true;
+    this.reset();
+    this.movieService.withError = false;
+  }
+
+  reset() {
+    this.state$ = this.createState();
+    this.streamedState$ = this.createStreamedState();
+  }
+
+  search(term: string) {
+    this.searchTerm$.next(term);
+  }
+
+  triggerSuspense() {
+    this.suspenseTrigger$.next();
+  }
+
+  triggerComplete() {
+    this.completeTrigger$.next();
+  }
+
+  triggerError() {
+    this.errorTrigger$.next();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { LetModule } from '@rx-angular/template/let';
 
 import { AppComponent } from './app.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { StarRatingModule } from './star-rating/star-rating.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, MovieCardComponent],
+  imports: [BrowserModule, StarRatingModule, LetModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
